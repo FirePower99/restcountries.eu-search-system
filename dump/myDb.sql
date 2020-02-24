9d40b441dc8fd4f97ae73b9cdb67387e8042135e
FirePower99 <n_daniels2004@hotmail.com>
2020-02-24 11:59:11 +0000

    Config Changes

--- dump/myDb.sql
+++ dump/myDb.sql
@@ -1,6 +1,4 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -9,15 +7,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `restcountries`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `countries`
---
 
 CREATE TABLE `countries` (
   `name` varchar(55) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
@@ -32,35 +21,12 @@ CREATE TABLE `countries` (
   `index` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `countries`
---
 
 INSERT INTO `countries` (`name`, `alpha2code`, `region`, `capital`, `callingcodes`, `timezones`, `languages`, `currencies`, `flag`, `index`) VALUES
 ('Singapore', 'SG', 'Asia', 'Singapore', '65', '\"UTC+08:00\"', 'English;Malay;Tamil;Chinese;', 'BND;SGD;', 'https://restcountries.eu/data/sgp.svg', 1),
 ('United Kingdom of Great Britain and Northern Ireland', 'GB', 'Europe', 'London', '44', '\"UTC-08:00\",\"UTC-05:00\",\"UTC-04:00\",\"UTC-03:00\",\"UTC-02:00\",\"UTC\",\"UTC+01:00\",\"UTC+02:00\",\"UTC+06:00\"', 'English', 'GBP;', 'https://restcountries.eu/data/gbr.svg', 2),
 ('United States of America', 'US', 'Americas', 'Washington, D.C.', '1', '\"UTC-12:00\",\"UTC-11:00\",\"UTC-10:00\",\"UTC-09:00\",\"UTC-08:00\",\"UTC-07:00\",\"UTC-06:00\",\"UTC-05:00\",\"UTC-04:00\",\"UTC+10:00\",\"UTC+12:00\"', 'English', 'USD;', 'https://restcountries.eu/data/usa.svg', 3);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `countries`
---
-ALTER TABLE `countries`
-  ADD UNIQUE KEY `index` (`index`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `countries`
---
-ALTER TABLE `countries`
-  MODIFY `index` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- www/index.php
+++ www/index.php
@@ -1,5 +1,27 @@
 <?php
 
-phpinfo();
+	$conn = mysqli_connect('db', 'root', 'test', "restcountries");
+
+	$query = 'SELECT * From countries';
+	$result = mysqli_query($conn, $query);
+
+
+    echo '<table class="table table-striped">';
+    echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
+    while($value = $result->fetch_array(MYSQLI_ASSOC)){
+        echo '<tr>';
+        echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
+        foreach($value as $element){
+            echo '<td>' . $element . '</td>';
+        }
+
+        echo '</tr>';
+    }
+    echo '</table>';
+
+    /* Libération du jeu de résultats */
+    $result->close();
+
+    mysqli_close($conn);
 
 ?>
